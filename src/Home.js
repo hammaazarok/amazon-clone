408297d44af522b062028829ae3a0d4297bc459d
Hamma Azarok <hammaagazarok@gmail.com>
2022-09-08 15:49:08 +0200

    add nav bar component

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./home.css"
+function Home() {
+  return (
+    <div className='home'>
+        <div className='home_container'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAe6FVWmL5nBy5IVWGWCYwzcLDb91XjpzU",
+    authDomain: "amaz-on-clone-ha.firebaseapp.com",
+    projectId: "amaz-on-clone-ha",
+    storageBucket: "amaz-on-clone-ha.appspot.com",
+    messagingSenderId: "959175896716",
+    appId: "1:959175896716:web:d4792b0b23ce5324f515a9",
+    measurementId: "G-6ZJN01WY59"
+  };
