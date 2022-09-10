d98b83df4a21f9b4eb0ee5f36975ac27d1e9ae0a
Hamma Azarok <hammaagazarok@gmail.com>
2022-09-10 16:09:09 +0200

    setup context API

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { StateProvider } from "./StateProvider";
+import { initialState, reducer } from "./reducer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <StateProvider initialState={initialState} reducer={reducer}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </StateProvider>
   </React.StrictMode>
 );
 
---
+++ src/reducer.js
@@ -0,0 +1,16 @@
+export const initialState = {
+  basket: [],
+};
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TO_BASKET":
+      return {
+        ...state,
+        basket: [...state.basket, action.item],
+      };
+
+    default:
+      return state;
+  };
+};
